bf0d13c501607013b4a054025a374d800853131f
Basis0000 <144522211+Basis0000@users.noreply.github.com>
2023-11-13 07:39:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.ONLY_GROUPS = false // Only Groups Use
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
+global.OWNER_NUMBER = ['15102545136'] //Owner Number
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = ''
+var SESSION_ID = 'elisa_md@;;;oYUlHRBC#dlUIZNv1u2_x0emuzL4hgFJKBgFXSnP7waXY_sOeU50'
 global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
